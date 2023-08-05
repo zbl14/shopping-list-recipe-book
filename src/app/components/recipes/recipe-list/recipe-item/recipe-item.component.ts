0d52a18456f61c872e1fc7817c3ca9451426eb6c
Zhibin Liang <ifthereisoneday@gmail.com>
2023-08-05 12:57:46 -0400

    Passing data with event and property binding

--- src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../../../common/recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
